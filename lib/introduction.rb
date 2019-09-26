cd8b64167b198c46ad45d247770ce639896e17de
Bethany Holley <chocolatechipfaced@gmail.com>
2019-09-26 19:45:31 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,15 +1,14 @@
-
-def greeting(name)
+def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def greeting_with_language("Beth" "Ruby")
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
-end
+#def introduction_with_language(name, language)
+#  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+#end
 
-def introduction_with_language_optional(name, language = "Ruby")
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
-end
+#def introduction_with_language_optional(name, language="Ruby")
+#  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+#end
 
 
 
